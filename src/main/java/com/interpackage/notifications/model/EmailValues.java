19bedf6286c6aa2af1d88dab404dca58d0e1df2c
BryanGmz <brgg13@gmail.com>
2023-05-12 16:48:01 -0600

    style: :memo: Solve warnings super-linter

--- src/main/java/com/interpackage/notifications/model/EmailValues.java
+++ src/main/java/com/interpackage/notifications/model/EmailValues.java
@@ -9,9 +9,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class EmailValues {
 
+    /** The email address of the sender */
     private String mailFrom;
+
+    /** The email address of the recipient */
     private String mailTo;
+
+    /** The subject of the email */
     private String subject;
+
+    /** The username associated with the email */
     private String username;
+
+    /** The message content of the email */
     private String message;
 }
