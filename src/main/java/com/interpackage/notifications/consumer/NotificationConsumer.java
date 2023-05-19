b8295d7006df89d642c8c9a8c22480ac041487e5
BryanGmz <brgg13@gmail.com>
2023-05-19 11:52:04 -0600

    feat: :sparkles: Added notification sending event when creating user

--- src/main/java/com/interpackage/notifications/consumer/NotificationConsumer.java
+++ src/main/java/com/interpackage/notifications/consumer/NotificationConsumer.java
@@ -1,20 +1,53 @@
-package com.interpackage.notifications.config;
+package com.interpackage.notifications.consumer;
 
 import com.interpackage.basedomains.dto.UserEvent;
+import com.interpackage.notifications.model.EmailValues;
+import com.interpackage.notifications.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Service
-public class UserConsumer {
-    
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserConsumer.class);
+public class NotificationConsumer {
 
-    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    /**
+     * `@Autowired` is a Spring annotation that injects an instance
+     * of the `EmailService` class into the `emailService` field.
+     * This allows the `EmailController` to use the methods
+     * provided by the `EmailService` class.
+     */
+    @Autowired
+    private EmailService emailService;
+    /** `@Value("${spring.mail.username}")` is a Spring annotation
+     * that injects the value of the property `spring.mail.username` from
+     * the application properties file into the `mailFrom` field.
+     * This allows the `EmailController` to use the email address
+     * specified in the application properties file as the sender
+     * of the email.
+     */
+    @Value("${spring.mail.username}")
+    private String mailFrom;
+
+    /**
+     * Método para consumir eventos de usuario desde Kafka.
+     *
+     * @param event el evento de usuario recibido desde Kafka
+     */
+    @KafkaListener(
+            topics = "${spring.kafka.topic.name}",
+            groupId = "${spring.kafka.email-consumer.group-id}")
     public void consume(UserEvent event){
-        LOGGER.info(String.format("UserEvent received => %s", event.toString()));
-
+        EmailValues dto = new EmailValues();
+        dto.setUsername(event.user.userName);
+        dto.setMailTo(event.user.email);
+        dto.setSubject("Correo de Bienvenida");
+        dto.setMessage("¡Bienvenido al equipo de InterPackageGT " + event.user.name + "!\n\n" +
+                "Estamos emocionados de tenerte como parte de nuestro equipo de trabajadores dedicados\n" +
+                "al envío de paquetes. Tu dedicación y habilidades serán fundamentales para brindar un\n" +
+                "servicio excepcional a nuestros clientes. Juntos, hagamos que cada envío cuente.\n" +
+                "\n\n" +
+                "¡Bienvenido y éxito en tu carrera con InterPackageGT!");
+        emailService.sendEmail(dto, mailFrom);
     }
-
 }
